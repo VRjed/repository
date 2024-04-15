2dba2090d57ff8f20d2839b1997e044c43996db3
VRjed <vavav4781@gmail.com>
2024-04-15 20:35:48 +1100

    1

--- ura/EGe/zadanie13/137.py
+++ ura/EGe/zadanie13/137.py
@@ -1,17 +1,5 @@
-#from ipaddress import *
-#c = 0
 print(bin(178)[2:])
 print(bin(165)[2:])
 
 print(24 + 4)
 #28
-
-
-
-
-#for i in range(32):
-    #n1 = ip_network('145.207.153.178/'+str(i),0)
-    #n2 = ip_network('145.207.153.165/'+str(i),0)
-   # p1 = str(n1).split('/')
-    #p2 = str(n2).split('/')
-    #if p[0]=='18.168.240.0':
